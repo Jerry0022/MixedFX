337c1f55edd5dad4ea4d6532bd2cfa3bf5b6ec13
Jerry0022 <Jeremy0022@gmail.com>
2015-04-21 23:53:01 +0200

    Included full gradle (with dependancy, src and javadoc) support.

---
+++ src/de/mixedfx/file/FileKA.java
@@ -0,0 +1,18 @@
+package de.mixedfx.file;
+
+import jfxtras.labs.scene.control.CalendarLabel;
+
+/**
+ * Cool thing!
+ *
+ * @author Jerry
+ *
+ */
+public class FileKA
+{
+	public static final CalendarLabel	c	= new CalendarLabel();
+
+	public FileKA()
+	{
+	}
+}
---
+++ src/de/mixedfx/file/Main.java
@@ -0,0 +1,11 @@
+package de.mixedfx.file;
+
+public class Main
+{
+
+	public static void main(final String[] args)
+	{
+
+	}
+
+}
