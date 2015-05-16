bf753a07cf0cf9a205c38e6df64c76fa6a5d20ac
Jerry0022 <Jeremy0022@gmail.com>
2015-05-16 11:28:32 +0200

    RegisteredMessage broadcast.

--- src/de/mixedfx/network/Connection.java
+++ src/de/mixedfx/network/Connection.java
@@ -154,7 +154,7 @@ public class Connection implements EventBusServiceInterface
 		if (message instanceof RegisteredMessage)
 		{
 			final RegisteredMessage regMessage = (RegisteredMessage) message;
-			if (regMessage.receivers.contains(ParticipantManager.MY_PID.get()))
+			if (regMessage.receivers.contains(ParticipantManager.MY_PID.get()) || regMessage.receivers.isEmpty())
 				EventBusExtended.publishAsyncSafe(MessageBus.MESSAGE_RECEIVE, message); // Publish
 			// internally
 		}
