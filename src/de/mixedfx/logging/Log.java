d7ca94335175f1e98026c84a66bcdd65047cab3e
Jerry0022 <Jeremy0022@gmail.com>
2015-07-19 17:32:39 +0200

    Tidied up.

--- src/de/mixedfx/logging/Log.java
+++ src/de/mixedfx/logging/Log.java
@@ -9,8 +9,9 @@ import org.apache.logging.log4j.core.LoggerContext;
 /**
  * Turn all Loggers on / off by calling {@link #turnAllOn()} or {@link #turnAllOff()}! Turn the
  * Logger off by calling {@link Logger#setLevel(Level)} with {@link Level#OFF}! See Level logic
- * here: <a href="http://www.tutorialspoint.com/log4j/log4j_logging_levels.htm"
- * >http://www.tutorialspoint.com/log4j/log4j_logging_levels.htm</a>
+ * here:
+ * <a href="http://www.tutorialspoint.com/log4j/log4j_logging_levels.htm" >http://www.tutorialspoint
+ * .com/log4j/log4j_logging_levels.htm</a>
  *
  * @author Jerry
  */
