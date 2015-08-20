921de1eeff74388dbdaf5e460a545ccd27c7fad6
Jerry <Jeremy0022@gmail.com>
2015-08-20 19:34:45 +0200

    Typo fix.

--- src/de/mixedfx/windows/AdminRequiredException.java
+++ src/de/mixedfx/windows/AdminRequiredException.java
@@ -1,10 +1,12 @@
 package de.mixedfx.windows;
 
-public class AdminRequiredException extends Exception {
+public class AdminRequiredException extends Exception
+{
 	/**
 	 * May start Eclipse as Admin!
 	 */
-	public AdminRequiredException() {
-		super("This program must be started as admin in ordert to work!");
+	public AdminRequiredException()
+	{
+		super("This program must be started as admin in order to work!");
 	}
 }
