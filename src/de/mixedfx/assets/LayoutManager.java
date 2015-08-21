f60914a1b07674727a6687d233ae684092814815
Jerry <Jeremy0022@gmail.com>
2015-08-21 18:43:08 +0200

    LayoutManager now prefers individual layouts but uses both style sheets

--- src/de/mixedfx/assets/LayoutManager.java
+++ src/de/mixedfx/assets/LayoutManager.java
@@ -170,7 +170,9 @@ public class LayoutManager
 		// Apply layout
 		final Collection<File> files = DataHandler.listFiles(layoutFullPath);
 		// Add files of standard folder if they aren't already in the specific layout folder
-		DataHandler.listFiles(this.standardLayoutDir).stream().filter(t -> !files.stream().anyMatch(u -> FileObject.create(t).getName().equalsIgnoreCase(FileObject.create(u).getName()))).forEach(s -> files.add(s));
+		DataHandler.listFiles(this.standardLayoutDir).stream()
+				.filter(t -> !files.stream().anyMatch(u -> FileObject.create(t).getName().equalsIgnoreCase(LayoutManager.STYLE_FILE_NAME) ? true : FileObject.create(t).getName().equalsIgnoreCase(FileObject.create(u).getName())))
+				.forEach(s -> files.add(s));
 		for (final File file : files)
 		{
 			if (FileObject.create(file).getName().equalsIgnoreCase(LayoutManager.STYLE_FILE_NAME))
