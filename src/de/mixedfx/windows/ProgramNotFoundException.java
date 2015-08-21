fb9504621fdda58d3127f491a94a1891eebbaccc
spike-rabbit <ralfrabbit@gmail.com>
2015-08-21 22:12:15 +0200

    fixed something

--- src/de/mixedfx/windows/ProgramNotFoundException.java
+++ src/de/mixedfx/windows/ProgramNotFoundException.java
@@ -1,6 +1,11 @@
 package de.mixedfx.windows;
 
 public class ProgramNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2162918659223506677L;
+
 	public ProgramNotFoundException(String message) {
 		super(message);
 	}
