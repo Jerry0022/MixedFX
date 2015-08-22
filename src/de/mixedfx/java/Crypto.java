5ddd932b530073c14dffe052ec5f082f79aef9f6
Jerry <Jeremy0022@gmail.com>
2015-08-22 16:28:52 +0200

    Improved Crypto

--- src/de/mixedfx/java/Crypto.java
+++ src/de/mixedfx/java/Crypto.java
@@ -1,8 +1,6 @@
 package de.mixedfx.java;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class Crypto
 {
@@ -10,20 +8,12 @@ public class Crypto
 	 *
 	 * @param text
 	 *            The String to hash. Should be in UTF-8 format!
-	 * @return Returns the hash of the string.
+	 * @return Returns the hash of the string as hex numbers (64 chars totally)!
 	 */
 	public static String getSHA256(final String text)
 	{
-		String result = "";
-		try
-		{
-			final MessageDigest md = MessageDigest.getInstance("SHA-256");
-			md.update(text.getBytes("UTF-8"));
-			final byte[] digest = md.digest();
-			result = new String(digest);
-		} catch (final UnsupportedEncodingException | NoSuchAlgorithmException e)
-		{
-		}
-		return result;
+		if ((text == null) || text.isEmpty())
+			throw new IllegalArgumentException("Only not null Strings with one or more characters can be processed!");
+		return DigestUtils.sha256Hex(text);
 	}
 }
