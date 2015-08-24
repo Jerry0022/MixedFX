393e69f50306ca556f66570d35f9adb82ad8e36c
Jerry <Jeremy0022@gmail.com>
2015-08-24 19:53:38 +0200

    Fixed test compile errors.

--- test/de/mixedfx/test/NetworkTester.java
+++ test/de/mixedfx/test/NetworkTester.java
@@ -9,22 +9,20 @@ import de.mixedfx.java.Ran;
 import de.mixedfx.logging.Log;
 import de.mixedfx.network.ConnectivityManager;
 import de.mixedfx.network.MessageBus;
-import de.mixedfx.network.MessageBus.MessageReceiver;
 import de.mixedfx.network.NetworkManager;
 import de.mixedfx.network.OverlayNetwork;
 import de.mixedfx.network.TCPClient;
+import de.mixedfx.network.UDPCoordinator;
 import de.mixedfx.network.UDPDetected;
-import de.mixedfx.network.messages.Message;
 import de.mixedfx.network.messages.WelcomeMessage;
 import de.mixedfx.network.user.User;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
 
 public class NetworkTester
 {
 
-	public static void main(String[] args)
+	public static void main(final String[] args)
 	{
 		/*
 		 * Set up logging!
@@ -33,129 +31,32 @@ public class NetworkTester
 		// Log.network.setLevel(Level.DEBUG);
 
 		// Log fatal errors (network reacted already to this error)
-		NetworkTester rebuild = new NetworkTester();
+		final NetworkTester rebuild = new NetworkTester();
 		AnnotationProcessor.process(rebuild);
 
 		Log.network.info("NOW TESTING NETWORK!");
 
 		// Log UDP members
-		NetworkManager.u.allAdresses.addListener(new ListChangeListener<UDPDetected>()
+		UDPCoordinator.allAdresses.addListener((ListChangeListener<UDPDetected>) c ->
 		{
-			@Override
-			public void onChanged(javafx.collections.ListChangeListener.Change<? extends UDPDetected> c)
-			{
-				c.next();
-				if (c.wasAdded())
-					Log.network.trace("AllAddresses updated: " + c.getAddedSubList().get(0));
-			}
+			c.next();
+			if (c.wasAdded())
+				Log.network.trace("AllAddresses updated: " + c.getAddedSubList().get(0));
 		});
 
 		// Log TCP members
-		NetworkManager.t.tcpClients.addListener(new ListChangeListener<TCPClient>()
+		NetworkManager.t.tcpClients.addListener((ListChangeListener<TCPClient>) c ->
 		{
-			@Override
-			public void onChanged(javafx.collections.ListChangeListener.Change<? extends TCPClient> c)
-			{
-				c.next();
-				if (c.wasRemoved())
-					for (TCPClient tcp : c.getRemoved())
-						Log.network.trace("TCP removed: " + tcp);
-				else if (c.wasAdded())
-					for (TCPClient tcp : c.getAddedSubList())
-						Log.network.debug("TCP added: " + tcp);
-			}
+			c.next();
+			if (c.wasRemoved())
+				for (final TCPClient tcp1 : c.getRemoved())
+					Log.network.trace("TCP removed: " + tcp1);
+			else if (c.wasAdded())
+				for (final TCPClient tcp2 : c.getAddedSubList())
+					Log.network.debug("TCP added: " + tcp2);
 		});
 
-		// Log users
-		ConnectivityManager.otherUsers.addListener((ListChangeListener<User>) c ->
-		{
-			while (c.next())
-			{
-				if (c.wasAdded())
-				{
-					User user = c.getAddedSubList().get(0);
-					Log.network.info((!c.wasReplaced() ? "New" : "Updated") + " User: " + user);
-					Log.network.info("User was detected to be in this network: " + user.networks);
-
-					OverlayNetwork network = user.networks.get(0);
-					Log.network.info("Network " + network + " has latency " + network.latencyProperty().get() + " ms!");
-					Log.network.info("Network " + network + " has reliability of " + Math.round(network.reliablityProperty().get() * 100) + "%!");
-					network.latencyProperty().addListener(new ChangeListener<Number>()
-					{
-						@Override
-						public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
-						{
-							Log.network.info("Network " + network + " has latency " + newValue.intValue() + " ms!");
-						}
-					});
-					network.reliablityProperty().addListener(new ChangeListener<Number>()
-					{
-						@Override
-						public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
-						{
-							Log.network.info("Network " + network + " has reliability of " + Math.round(newValue.doubleValue() * 100) + "%!");
-						}
-					});
-
-					user.networks.addListener(new ListChangeListener<OverlayNetwork>()
-					{
-
-						@Override
-						public void onChanged(javafx.collections.ListChangeListener.Change<? extends OverlayNetwork> c)
-						{
-							c.next();
-							if (c.wasAdded())
-							{
-								Log.network.info("User joined over other network: " + c.getAddedSubList().get(0) + " so that he is now in following networks: " + user.networks);
-								OverlayNetwork network = c.getAddedSubList().get(0);
-								network.latencyProperty().addListener(new ChangeListener<Number>()
-								{
-
-									@Override
-									public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
-									{
-										Log.network.info("Network " + network + " has latency " + newValue.intValue() + " ms!");
-									}
-								});
-								network.reliablityProperty().addListener(new ChangeListener<Number>()
-								{
-									@Override
-									public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
-									{
-										Log.network.info("Network " + network + " has reliability of " + Math.round(newValue.doubleValue() * 100) + "%!");
-									}
-								});
-							} else
-								Log.network.info("User left over this network: " + c.getRemoved().get(0) + " but is still in: " + user.networks);
-						}
-
-					});
-					WelcomeMessage message = new WelcomeMessage();
-					message.setReceivers(user);
-					MessageBus.send(message);
-				} else if (c.wasRemoved())
-					Log.network.info("Removed User: " + c.getRemoved().get(0));
-			}
-
-			Log.network.info("UserManager list changed to: " + ConnectivityManager.otherUsers);
-		});
-
-		MessageBus.registerForReceival(new MessageReceiver()
-		{
-			@Override
-			public void receive(Message message)
-			{
-				if (message instanceof WelcomeMessage)
-					Log.network.info("YEAH! WelcomeMessage received!");
-			}
-		}, true);
-
-		/*
-		 * Set up network!
-		 */
-
-		// Create example user
-		ConnectivityManager.start(new User()
+		final ConnectivityManager<User> con = new ConnectivityManager<User>(new User()
 		{
 			private String id;
 
@@ -166,11 +67,11 @@ public class NetworkTester
 			@Override
 			public Object getIdentifier()
 			{
-				return id;
+				return this.id;
 			}
 
 			@Override
-			public void mergeMe(User newUser)
+			public void mergeMe(final User newUser)
 			{
 			}
 
@@ -179,10 +80,62 @@ public class NetworkTester
 			{
 			}
 		});
+
+		// Log users
+		con.otherUsers.addListener((ListChangeListener<User>) c ->
+		{
+			while (c.next())
+			{
+				if (c.wasAdded())
+				{
+					final User user = c.getAddedSubList().get(0);
+					Log.network.info((!c.wasReplaced() ? "New" : "Updated") + " User: " + user);
+					Log.network.info("User was detected to be in this network: " + user.networks);
+
+					final OverlayNetwork network = user.networks.get(0);
+					Log.network.info("Network " + network + " has latency " + network.latencyProperty().get() + " ms!");
+					Log.network.info("Network " + network + " has reliability of " + Math.round(network.reliablityProperty().get() * 100) + "%!");
+					network.latencyProperty().addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> Log.network.info("Network " + network + " has latency " + newValue.intValue() + " ms!"));
+					network.reliablityProperty().addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> Log.network.info("Network " + network + " has reliability of " + Math.round(newValue.doubleValue() * 100) + "%!"));
+
+					user.networks.addListener((ListChangeListener<OverlayNetwork>) c1 ->
+					{
+						c1.next();
+						if (c1.wasAdded())
+						{
+							Log.network.info("User joined over other network: " + c1.getAddedSubList().get(0) + " so that he is now in following networks: " + user.networks);
+							final OverlayNetwork network1 = c1.getAddedSubList().get(0);
+							network1.latencyProperty().addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> Log.network.info("Network " + network1 + " has latency " + newValue.intValue() + " ms!"));
+							network1.reliablityProperty().addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> Log.network.info("Network " + network1 + " has reliability of " + Math.round(newValue.doubleValue() * 100) + "%!"));
+						} else
+							Log.network.info("User left over this network: " + c1.getRemoved().get(0) + " but is still in: " + user.networks);
+					});
+					final WelcomeMessage message = new WelcomeMessage();
+					message.setReceivers(user);
+					MessageBus.send(message);
+				} else if (c.wasRemoved())
+					Log.network.info("Removed User: " + c.getRemoved().get(0));
+			}
+
+			Log.network.info("UserManager list changed to: " + con.otherUsers);
+		});
+
+		MessageBus.registerForReceival(message ->
+		{
+			if (message instanceof WelcomeMessage)
+				Log.network.info("YEAH! WelcomeMessage received!");
+		} , true);
+
+		/*
+		 * Set up network!
+		 */
+
+		// Create example user
+		con.start();
 		try
 		{
 			Thread.sleep(Ran.dom(30000, 30000));
-		} catch (InterruptedException e)
+		} catch (final InterruptedException e)
 		{
 		}
 		// ConnectivityManager.stop();
@@ -198,7 +151,7 @@ public class NetworkTester
 	}
 
 	@EventTopicSubscriber(topic = NetworkManager.NETWORK_FATALERROR)
-	public void on(String topic, Exception data)
+	public void on(final String topic, final Exception data)
 	{
 		Log.network.fatal("NETWORK FATAL ERROR: " + data);
 	}
--- test/de/mixedfx/test/VoiceButton.java
+++ test/de/mixedfx/test/VoiceButton.java
@@ -1,8 +1,5 @@
 package de.mixedfx.test;
 
-import com.darkprograms.speech.recognizer.GSpeechResponseListener;
-import com.darkprograms.speech.recognizer.GoogleResponse;
-
 import de.mixedfx.assets.ImageProducer;
 import de.mixedfx.gui.RegionManipulator;
 import de.mixedfx.textandspeech.IncorrectInputLanguage;
@@ -13,15 +10,12 @@ import de.mixedfx.textandspeech.TextToSpeech;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class VoiceButton extends Pane
 {
-	private BooleanProperty voiceSwitcher = new SimpleBooleanProperty(false);
+	private final BooleanProperty voiceSwitcher = new SimpleBooleanProperty(false);
 
 	public VoiceButton()
 	{
@@ -30,71 +24,50 @@ public class VoiceButton extends Pane
 		this.setMaxSize(50, 50);
 		this.setStyle("-fx-cursor: hand");
 
-		// Attention: This action may need some seconds!
-		TextToSpeech.init();
-
 		// Set TTS config
-		TTSConfig config = new TTSConfig();
+		final TTSConfig config = new TTSConfig();
 		config.lang = Language.GERMAN;
 		config.male = false;
 		config.block = true;
 
 		// TODO Get username from config
-		String userName = "Jeremy";
+		final String userName = "Jeremy";
 
-		voiceSwitcher.addListener(new ChangeListener<Boolean>()
-		{
-			@Override
-			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue)
-			{
-				// Change style of this!
-				RegionManipulator.bindBackground(VoiceButton.this, newValue.booleanValue() ? ImageProducer.getMonoColored(Color.GREEN) : ImageProducer.getMonoColored(Color.BLUE));
-			}
-		});
+		this.voiceSwitcher.addListener(
+				(ChangeListener<Boolean>) (observable, oldValue, newValue) -> RegionManipulator.bindBackground(VoiceButton.this, newValue.booleanValue() ? ImageProducer.getMonoColored(Color.GREEN) : ImageProducer.getMonoColored(Color.BLUE)));
 
-		this.setOnMouseClicked(new EventHandler<MouseEvent>()
-		{
-			@Override
-			public void handle(MouseEvent event)
-			{
-				voiceSwitcher.set(voiceSwitcher.get() ? false : true);
-			}
-		});
+		this.setOnMouseClicked(event -> VoiceButton.this.voiceSwitcher.set(VoiceButton.this.voiceSwitcher.get() ? false : true));
 
 		// Incoming voice?
 		SpeechToText.language = Language.ENGLISH;
 		try
 		{
-			SpeechToText.startListening("AIzaSyBI012J_HtuCeXTpwvcaNB3awo88uAbIGA", voiceSwitcher, new GSpeechResponseListener()
+			SpeechToText.startListening("AIzaSyBI012J_HtuCeXTpwvcaNB3awo88uAbIGA", this.voiceSwitcher, gr ->
 			{
-				@Override
-				public void onResponse(GoogleResponse gr)
+				System.out.println("Antwort von Google: " + gr.getResponse());
+				System.out.println("Jetzt auf Deutsch");
+				try
 				{
-					System.out.println("Antwort von Google: " + gr.getResponse());
-					System.out.println("Jetzt auf Deutsch");
+					TextToSpeech.say("Hallo " + userName + ", du hast gerade gesagt: " + gr.getResponse(), config);
+				} catch (final IncorrectInputLanguage e2)
+				{
 					try
 					{
-						TextToSpeech.say("Hallo " + userName + ", du hast gerade gesagt: " + gr.getResponse(), config);
-					} catch (IncorrectInputLanguage e)
-					{
-						try
-						{
-							TextToSpeech.say("Es tut mir Leid, aber ich habe dich nicht verstanden!", config);
-						} catch (IncorrectInputLanguage e1)
-						{
-						}
-					}
-					System.out.println("Jetzt auf Englisch");
-					config.lang = Language.ENGLISH;
-					try
-					{
-						TextToSpeech.say("Hello " + userName + "! you have said " + gr.getResponse(), config);
-					} catch (IncorrectInputLanguage e)
+						TextToSpeech.say("Es tut mir Leid, aber ich habe dich nicht verstanden!", config);
+					} catch (final IncorrectInputLanguage e1)
 					{
 					}
 				}
+				System.out.println("Jetzt auf Englisch");
+				config.lang = Language.ENGLISH;
+				try
+				{
+					TextToSpeech.say("Hello " + userName + "! you have said " + gr.getResponse(), config);
+				} catch (final IncorrectInputLanguage e3)
+				{
+				}
 			});
-		} catch (Exception e)
+		} catch (final Exception e)
 		{
 			System.out.println("No micro found!");
 		}
