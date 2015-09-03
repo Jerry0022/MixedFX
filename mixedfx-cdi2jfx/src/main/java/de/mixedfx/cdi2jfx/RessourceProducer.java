6129c16920696638df62a3a0a9c0d6c5b4934aef
spike-rabbit <ralfrabbit@gmail.com>
2015-09-03 18:35:25 +0200

    correct building of controller
    return the controller and not the CDI-Proxy

--- mixedfx-cdi2jfx/src/main/java/de/mixedfx/cdi2jfx/RessourceProducer.java
+++ mixedfx-cdi2jfx/src/main/java/de/mixedfx/cdi2jfx/RessourceProducer.java
@@ -1,7 +1,11 @@
 package de.mixedfx.cdi2jfx;
 
-import javax.enterprise.inject.Instance;
+import javax.enterprise.context.spi.Contextual;
+import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
 
 import javafx.fxml.FXMLLoader;
@@ -9,16 +13,20 @@ import javafx.util.Callback;
 
 public class RessourceProducer {
     @Inject
-    private Instance<Object> instancer;
+    private BeanManager manager;
 
     @Produces
     public FXMLLoader buildLoader() {
 	FXMLLoader loader = new FXMLLoader();
 	loader.setControllerFactory(new Callback<Class<?>, Object>() {
 
+	    @SuppressWarnings("unchecked")
 	    @Override
 	    public Object call(Class<?> param) {
-		return RessourceProducer.this.instancer.select(param);
+		CDI.current().select(param).get();
+		Bean<?> bean = RessourceProducer.this.manager.getBeans(param).iterator().next();
+		CreationalContext<Object> ctx = RessourceProducer.this.manager.createCreationalContext(null);
+		return RessourceProducer.this.manager.getContext(bean.getScope()).get((Contextual<Object>) bean, ctx);
 	    }
 
 	});
