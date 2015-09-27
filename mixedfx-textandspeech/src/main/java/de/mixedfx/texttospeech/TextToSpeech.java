303e6ab31a527f2fdc111273b56b7e0e72becf28
Indi101 <Indi101@web.de>
2015-09-27 20:49:41 +0200

    TTS Voice loading... not complete

--- mixedfx-textandspeech/src/main/java/de/mixedfx/texttospeech/TextToSpeech.java
+++ mixedfx-textandspeech/src/main/java/de/mixedfx/texttospeech/TextToSpeech.java
@@ -1,12 +1,20 @@
 package de.mixedfx.texttospeech;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
 import java.util.Locale;
 
 import javax.sound.sampled.AudioInputStream;
+import javax.xml.crypto.Data;
 
 import com.detectlanguage.DetectLanguage;
 import com.detectlanguage.errors.APIError;
 
+import de.mixedfx.file.DataHandler;
+import de.mixedfx.file.FileObject;
 import de.mixedfx.logging.Log;
 import de.mixedfx.speechtotext.IncorrectInputLanguage;
 import marytts.LocalMaryInterface;
@@ -14,10 +22,47 @@ import marytts.MaryInterface;
 import marytts.exceptions.MaryConfigurationException;
 import marytts.exceptions.SynthesisException;
 import marytts.util.data.audio.AudioPlayer;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.UnzipParameters;
+import org.apache.commons.io.FileUtils;
 
 public class TextToSpeech
 {
 	private static AudioPlayer	audioPlayer;
+
+    public static void loadVoices() throws ZipException, IOException {
+        for(Language lang : Language.values())
+            for(int male=0; male<2; male++)
+            {
+                // Built-in language
+                if(lang.equals(Language.ENGLISH) && male==0)
+                    continue;
+                // Already available?
+                if(new File("voice-"+lang.getVoice(male==0?false:true)+".jar").exists())
+                    continue;
+                else
+                    System.out.println("DO Lang: " + lang);
+
+                String filename = "voice-"+lang.getVoice(male==0?false:true)+"-5.1.zip";
+                File file = new File(filename);
+                FileUtils.copyURLToFile(new URL("http://mary.dfki.de/download/5.1/"+filename), file);
+                ZipFile zipFile = new ZipFile(filename);
+                zipFile.extractAll("./voices");
+            }
+            FileObject zipDirectory = FileObject.create().setPath("voices\\lib\\");
+            Collection<File> files = DataHandler.listFiles(zipDirectory);
+            for(File f : files)
+            {
+                FileObject endFile = FileObject.create(f).setPath(".");
+                FileUtils.copyFile(f, endFile.toFile());
+                DataHandler.deleteFile(endFile.setExtension("zip"));
+                FileUtils.moveFile(endFile.toFile(), endFile.setName(endFile.getName().substring(0, endFile.getName().length()-4)).toFile());
+            }
+            DataHandler.deleteFile(FileObject.create().setPath("voices"));
+            // TODO Jerry In which folder? MaryTTS don't recognize them...
+    }
+
 
 	/**
 	 *
