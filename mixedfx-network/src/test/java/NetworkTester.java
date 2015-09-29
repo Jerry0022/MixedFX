b7a3bc097fb5bbadc60fa93cf4dbbf09c3741c99
Indi101 <Indi101@web.de>
2015-09-29 12:43:59 +0200

    Reprocess resources and NetworkTester improvement

--- mixedfx-network/src/test/java/NetworkTester.java
+++ mixedfx-network/src/test/java/NetworkTester.java
@@ -1,6 +1,8 @@
 import de.mixedfx.network.ConnectivityManager;
 import de.mixedfx.network.user.User;
 
+import java.util.UUID;
+
 /**
  * Created by Jerry on 25.09.2015.
  */
@@ -8,9 +10,13 @@ public class NetworkTester {
     public static void main(String[] args) {
         ConnectivityManager manager = new ConnectivityManager();
         manager.start(new User() {
+            private String id;
+            {
+                this.id = UUID.randomUUID().toString();
+            }
             @Override
             public Object getIdentifier() {
-                return null;
+                return this.id;
             }
 
             @Override
